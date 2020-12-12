65247d2436575ae8eb30227ce048532303c91d87
ksy336 <65062520+ksy336@users.noreply.github.com>
2020-12-12 18:24:32 +0400

    Add files via upload

---
+++ _tests_/utils/countDown.test.js
@@ -0,0 +1,15 @@
+import { countDown } from "../../utils/countDown.js";
+
+describe("tests for countDown function", () => {
+   
+
+ it ('should operate correctly with number  ', () => {
+     expect(countDown(even number)).toBe('Number${even number} is an even number');
+    }),
+ it('should operate correctly with ', () => {
+     expect(countDown(negative number)).toBe('Number${negative number} is a negative number');
+ }),
+ it('should operate correctly with invalid data', () => {
+     expect(countDown('love')).toBe('Data is invalid);
+  }),    
+})
