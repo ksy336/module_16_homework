af324e7e0e72b9f2795dbd08e57d9a894b8521a3
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2020-12-28 16:40:09 +0300

    Проверено

--- _tests_/utils/countDown.test.js
+++ _tests_/utils/countDown.test.js
@@ -1,15 +1,18 @@
-import { countDown } from "../../utils/countDown.js";
+import { countDown } from "../../utils/countDown";
 
 describe("tests for countDown function", () => {
-   
-
- it ('should operate correctly with number  ', () => {
-     expect(countDown(even number)).toBe('Number${even number} is an even number');
-    }),
- it('should operate correctly with ', () => {
-     expect(countDown(negative number)).toBe('Number${negative number} is a negative number');
- }),
- it('should operate correctly with invalid data', () => {
-     expect(countDown('love')).toBe('Data is invalid);
-  }),    
+    it ('should operate correctly with number', () => {
+        expect(countDown(4)).toBe('4, 3, 2, 1');
+    })
+    it('should operate correctly with number in text format', () => {
+        expect(countDown('3')).toBe('3, 2, 1');
+    })
+    it('should operate correctly with 1', () => {
+        expect(countDown(1)).toBe('1');
+    })
+    it('should operate correctly with invalid data', () => {
+        expect(countDown('love')).toBe('');
+    })   
 })
+
+// Тесты написаны неправильно. В функцию нужно передавать конкретные значения, и проверять также конкретные значения, которые вернет функция
